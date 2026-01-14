055b882979a1956d59157e71eb391ec1617717f4
AllSystem88 <deependray228@gmail.com>
2026-01-14 14:07:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,14 +89,14 @@ confirmer = {}
 file_cache: dict[str, float] = {}
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/zk5w01.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/7wojv6.jpg"
 )
     
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://files.catbox.moe/zk5w01.jpg"
 )
 PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-STATS_IMG_URL = "https://files.catbox.moe/zk5w01.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/7wojv6.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
 STREAM_IMG_URL = "https://graph.org/file/ff2af8d4d10afa1baf49e.jpg"
